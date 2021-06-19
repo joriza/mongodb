1e5a628867cd8bd22fd6ee4ce12af94815889004
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2021-06-19 09:45:24 -0300

    primer commit

---
+++ primeraConexion.py
@@ -0,0 +1,18 @@
+# Para ejecutar Mongo en Docker
+# docker run -d -p 27017:27017 --name mydatabase mongo:4.2
+
+import pymongo
+
+from pymongo import MongoClient
+
+# conexión
+client = MongoClient('localhost',27017)
+db = client.mitienda
+
+# colección
+cuentas = db.usuarios
+
+resultado = db.usuarios.find()
+
+# Imprime las bases de datos existentes
+print(client.list_database_names())
---
+++ segundaConexion.py
@@ -0,0 +1,35 @@
+    # Para ejecutar Mongo en Docker
+# docker run -d -p 27017:27017 --name mydatabase mongo:4.2
+# hasta que recuerde mejor el uso de docker, para poder usar nuevamente el contenedor
+# Primero se debe borrar el contenedor con el comando
+# docker rm <nombre>
+
+import os
+import pymongo
+from pymongo import MongoClient
+os.system ("clear")
+
+# conexión
+client = MongoClient('localhost',27017)
+#crear una base vacía, #Si existe la abre
+db = client['prueba']
+# crea una colección ,si existe la usa
+col = db['personas']
+# Imprime las bases de datos existentes
+print(client.list_database_names())
+# Imprime las colecciones de esas bases
+print(db.list_collection_names())
+
+#Recuento de documentos
+cant = col.count_documents({})
+print("cantidad de registros:", cant)
+
+#Recorrer todos los documentos
+for documento in col.find({}):
+    print(documento)
+
+print("coleccion en crudo:\n",col)
+
+registro = {"nombre":"jorge","intereses":["dato1","dato2"]}
+
+col.insert_one(registro)
