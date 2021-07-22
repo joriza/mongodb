b66a0bac56255f4da8fcc04a28220d8d6803d08b
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2021-07-22 00:56:12 -0300

    210722_0056

---
+++ completo-objetos.py
@@ -0,0 +1,76 @@
+# Para ejecutar Mongo en Docker
+# docker run -d -p 27017:27017 --name mydatabase mongo:4.2
+# docker run --rm -d -p 27017:27017 --name mongo mongo:4.2
+# docker start mydatabase
+# hasta que recuerde mejor el uso de docker, para poder usar nuevamente el contenedor
+# Primero se debe borrar el contenedor con el comando
+# docker rm <nombre>
+
+import os
+import json
+import pymongo
+from pymongo import MongoClient
+os.system ("clear")
+
+class Basededatos():
+  def __init__(self):
+      self.client = MongoClient('localhost',27017,username="root",password="root")
+      self.db = self.client['pru']
+      self.col = self.db['temp']
+
+  def imprime(self):
+      print("\nDesde metodo imprime")
+      cant = self.col.count_documents({})
+      print("cantidad de registros:", cant)
+      for documento in self.col.find({},{"_id":0}): # imprime sin el :id
+          print(documento)
+
+  def agrega(self):
+    registro = {"nombre":"jorge","intereses":["dato1","dato2"]}
+    self.col.insert_one(registro)
+
+  def RequiredDomains(self,file):
+    file = file+".json"
+    print("archivo: ",file)
+    with open("RequiredDomains.json", "r") as read_file:
+      registro = json.load(read_file)
+    self.col.insert_one(registro)
+
+  def exporta(self):
+    pass
+
+
+if __name__ == '__main__':
+  # opcion=input("Ingrese Opcion:")
+  # print(opcion)
+
+  # datos = Basededatos()
+  # datos.agrega()
+  # print("desde main\n",datos.client)
+  # datos.RequiredDomains("data_file")
+  # datos.imprime()
+
+  ans=True
+  while ans:
+      print ("""
+      1.Add a Student
+      2.Delete a Student
+      3.Look Up Student Record
+      4.Exit/Quit
+      """)
+      ans=input("What would you like to do? ")
+      if ans=="1":
+        print("\n Student Added")
+      elif ans=="2":
+        print("\n Student Deleted")
+      elif ans=="3":
+        print("\n Student Record Found")
+      elif ans=="4":
+        print("\n Goodbye")
+      elif ans !="":
+        print("\n Opción inválida")
+
+
+# registro = {"nombre":"jorge","intereses":["dato1","dato2"]}
+# col.insert_one(registro)
+
