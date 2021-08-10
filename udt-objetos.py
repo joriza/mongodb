8c73b0be4152fd5219f2738e3091ade94a4e6106
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2021-08-10 20:00:48 -0300

    210810_2000

--- udt-objetos.py
+++ udt-objetos.py
@@ -1,6 +1,6 @@
 import os
 import json
-import jmespath
+import copy
 
 qry_input = "frases.txt" # Archivo con las frases origen
 
@@ -17,68 +17,38 @@ class EstructuraUDT():
     input("Presione Enter para continuar...")
 
   def AbreArchivoOrigen(self,sourcefile):
+    '''Abre archivo que contiene las frases a procesar'''
     aux = open(sourcefile, "r")
     return(aux)
 
-  def LeeJsonModelo(self,param1):
-    '''Lee archivo json pasado por parámetro
+  def LeeJsonModelo(self,filename):
+    '''Lee archivo json Modelo pasado por parámetro
     y devuelve lo leido en un objeto'''
-    with open(param1, "r") as read_file:
+    with open(filename, "r") as read_file:
       docJson = json.load(read_file)
     return(docJson)
 
-  def GrabaJsonSalida(self,param1):
-    '''Graba a archivo json
-    objeto pasado por parámetro'''
-    with open("data_dump.json", "w") as write_file: # Escribe en archivo json indentado
-      json.dump(param1, write_file, ensure_ascii= False, indent=2)
+  def GrabaJsonSalida(self,object,filename):
+    '''Graba en archivo json de salida
+    el objeto pasado por parámetro'''
+    with open(filename, "w") as write_file: # Escribe en archivo json indentado
+      json.dump(object, write_file, ensure_ascii= False, indent=2)
     # return(docJson)
 
 if __name__ == '__main__':
   udt = EstructuraUDT()
   frases = udt.AbreArchivoOrigen(qry_input)
-
   jsonModelo = udt.LeeJsonModelo("jsonModeloCortoMPC.json")
+  TestCaseModelo = jsonModelo['TestCases'][0]  ## Extrae el primer testcase para reutilizar
+
   for frase_actual in frases:
     frase_actual= frase_actual. rstrip("\n")
-    print(frase_actual)
+    # print(frase_actual)
+    nuevoTestCase = copy.deepcopy(TestCaseModelo) ## Ojo al copiar un diccionario o lista
+    nuevoTestCase['Input'][0]['Input'] = frase_actual ## Reemplaza la frase en el testcase_modelo recurrente
+    jsonModelo['TestCases'].append(nuevoTestCase) ## Agrega un nuevo test_case a la lista de testcases
 
-  # acond_expected = jsonModelo
-  # acond_expected = acond_expected.replace('FraseModelo','frase_actual')
+  jsonModelo['TestCases'].pop(0) ## Se elimina el 1er test case que es que se usó de modelo.
+  udt.GrabaJsonSalida(jsonModelo,"salida.json")
 
-
-  print("\nTodo el json: ",jsonModelo)
-  print("todo el json: ", type(jsonModelo))
-  # jsonModelo['TestCases'][0]['Input'][0]['Input'] = frase_actual
-  # print("Frase extraida: ",a)
-  nuevoTestCase = jsonModelo['TestCases'][0]
-  print("\nnuevoTestCase: ",nuevoTestCase)
-  print(type(nuevoTestCase))
-
-
-  testCasesActuales = jsonModelo['TestCases']
-  print("\n los test Cases: ",testCasesActuales)
-  print(type(testCasesActuales))
-
-  jsonModelo['TestCases'].append(nuevoTestCase)
-  print("\n json Modelo Final: ",jsonModelo)
-
-  udt.GrabaJsonSalida(jsonModelo)
-
-  # lst_resultado.append(param1) ## agrega item a un a lista
-  # dic_resultado.update(param1) ## agregar item a un dicc
-
-
-  # # Extrae plantilla TestCases
-  # aux0 = jmespath.search('TestCases[].Input[]', jsonModelo)
-
-  # print("tipo dato list TestCases.Input: ",type(jsonModelo))
-  # aux = jsonModelo[0]
-  # print("tipo dato dict TestCases.Input: ",type(aux))
-  # print("TestCases.Input: ",aux)
-  # print(aux['Input'])
-  # aux2 = jmespath.search('[].Input[]', jsonModelo)
-  # print("aux2",aux2)
-  # print("Con Json Py",.Input[])
-
-  print("\nTerminado normalente")
+  print("Generados ",len(jsonModelo['TestCases'])," test")
