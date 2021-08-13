bc9c7bcc9f450d1b50e709063faefcb711469a6c
Jorge J. IZAGUIRRE <jizaguirre@huenei.com>
2021-08-12 22:08:24 -0300

    210812_2208

--- udt-objetos2.py
+++ udt-objetos2.py
@@ -43,17 +43,24 @@ if __name__ == '__main__':
   jsonModelo['TestCases'].clear() ## Elimina los test cases del json modelo.
 
   for frase_actual in frases:
-    # print(row['Query'])
-    # print(row['Expected Json Result'])
     nuevoTestCase = copy.deepcopy(TestCaseModelo) ## Ojo al copiar un diccionario o lista
     nuevoTestCase['Input'][0]['Input'] = frase_actual['Query'] ## Reemplaza la frase en el testcase_modelo recurrente
-    nuevoTestCase['Expected'][0]['JSON'] = frase_actual['Expected Json Result'] ## Reemplaza la frase en el testcase_modelo recurrente
+    nuevoTestCase['Input'][0]["kpic_extra_info"]["query_nr"] = frase_actual['Query#'] ## Reemplaza el nro de Query en el testcase_modelo recurrente
+    nuevoTestCase['Input'][0]["kpic_extra_info"]["level_subset"] = frase_actual['Level'] ## Reemplaza el nivel en el testcase_modelo recurrente
+    nuevoTestCase['Expected'][0]['JSON'].clear() # Vacío Expected para que luego pueda hacer un appen sin problemas
+    # Acondiciono expected (aun en modo texto)
+    acond_expected= frase_actual['Expected Json Result']
+    acond_expected = acond_expected.replace('AllResults','ServerResponse')
+    acond_expected = acond_expected.replace('[','')
+    acond_expected = acond_expected.replace(']','')
+    # Fin Acondiciono expected
+    nuevoTestCase['Expected'][0]['JSON'].append(json.loads(acond_expected)) ## Reemplaza el ExpextedJson en el Json
     jsonModelo['TestCases'].append(nuevoTestCase) ## Agrega un nuevo test_case a la lista de testcases
 
-    print(frase_actual['Expected Json Result'])
-    print(json.dumps(frase_actual['Expected Json Result'],indent=2))
+  aux = nuevoTestCase['Input'][0]["kpic_extra_info"]["query_nr"]# = frase_actual['Query#'] ## Reemplaza la frase en el testcase_modelo recurrente
+  print("aux",aux)
 
-    quit()
+    # quit()
 
   udt.SaveTargetFile(jsonModelo,"salida.json")
   print("Generados",len(jsonModelo['TestCases']),"test")
